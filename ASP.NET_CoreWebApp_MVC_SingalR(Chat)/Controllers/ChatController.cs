c908e6dc7b2b4aa310a2d0f6e3206ebff955ee73
Guillermo Riaño <guilledark17@gmail.com>
2023-07-03 12:07:13 -0600

    ✅ Chat Controller

---
+++ ASP.NET_CoreWebApp_MVC_SingalR(Chat)/Controllers/ChatController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_CoreWebApp_MVC_SingalR_Chat_.Controllers
+{
+    public class ChatController : Controller
+    {
+        // Dictionary static
+        public static Dictionary<int, string> Rooms = new Dictionary<int, string>() {
+            {1, "Develop"},
+            {2, "Video Game"},
+            {3, "Technology"},
+        };
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Room(int room)
+        {
+            return View("Roon", room);
+        }
+    }
+}
