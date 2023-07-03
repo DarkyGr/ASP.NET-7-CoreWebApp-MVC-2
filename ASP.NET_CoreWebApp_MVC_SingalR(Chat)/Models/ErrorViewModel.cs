a29133ecd59e1836d63d4e83940ae3985924fe3a
Guillermo Riaño <guilledark17@gmail.com>
2023-07-03 11:18:00 -0600

    🎉 Initial: Adding Project Folder

---
+++ ASP.NET_CoreWebApp_MVC_SingalR(Chat)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_CoreWebApp_MVC_SingalR_Chat_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
