0e89a7de19ce8345be482b85fe72cd47ac52ae9d
Guillermo Riaño <guilledark17@gmail.com>
2023-07-03 11:58:54 -0600

    ✅ Chat Hub

---
+++ ASP.NET_CoreWebApp_MVC_SingalR(Chat)/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ASP.NET_CoreWebApp_MVC_SingalR_Chat_
+{
+    public class ChatHub : Hub
+    {
+        // Send Method
+        public async Task Send(string room, string user, string message)
+        {
+            // Who receive the message
+            await Clients.Group(room).SendAsync("ReceiveMessage", user, message);
+        }
+
+        // Send Method
+        public async Task AddToGroup(string room)
+        {
+            // Add to group
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+
+            // Who did connect
+            await Clients.Group(room).SendAsync("ShowWho", $"Someone did connect {Context.ConnectionId}");
+        }
+
+    }
+}
